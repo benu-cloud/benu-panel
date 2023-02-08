7e1c552ce858e0a469b8bbacd6d291c3a3449e9c
Javad Jazandari <pash.u2048@gmail.com>
2023-02-08 11:33:25 +0330

    add sentry to code base

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import router from './Routes/index';
 import './index.css';
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'development') {
+  // production stage enable sentry
+  Sentry.init({
+    dsn: "https://c07bb11a15784765a460cd307555c43e@sentry.hamravesh.com/5132",
+    integrations: [new BrowserTracing()],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0,
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
